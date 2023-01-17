2d016ec1be9a00232b5e83dd69e9e9f0ef119b84
xZer0x17 <111791370+xZer0x17@users.noreply.github.com>
2023-01-17 22:54:32 +0100

    4º commit
    Falta solo el de character y string

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,63 @@
 /*   By: alflores <alflores@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 20:04:19 by alflores          #+#    #+#             */
-/*   Updated: 2023/01/17 21:15:13 by alflores         ###   ########.fr       */
+/*   Updated: 2023/01/17 22:53:24 by alflores         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-//Para printear:
-int	ft_putchar(char c)
-{
-	return(write(1, &c, 1));
-}
+//Para printear el string de forma "tradicional"
+// int ft_printf_string(char const *str)
+// {
+// 	return(write(1, str, ft_strlen(str)));
+// }
+//Para comprobar si es negativo:
+// static int	ft_negnb(int nb)
+// {
+// 	if (nb < 0)
+// 		return (-nb);
+// 	return nb;
+// }
+//Para ver el tamaño del número entero:
+// static	int ft_nblen(int nb)
+// {
+// 	int	len;
+
+// 	len = 0;
+// 	if (nb < 0)
+// 		len++;
+// 	while (nb !=  0)
+// 	{
+// 		nb /= 10;
+// 		len ++;
+// 	}
+// 	return len;
+// }
+//Para printear el numero decimal en base 10
+// int	ft_printf_d(int nb)
+// {
+// 	char	*str;
+// 	int		i;
+// 	int		n;
+// 	int		aux;
+
+// 	i = ft_nblen(nb);
+// 	n = ft_negnb(nb);
+// 	aux = i;
+// 	str = (char *)malloc(sizeof(char) * (i + 1));
+// 	str[i--] = '\0';
+// 	while (i >= 0)
+// 	{
+// 		if (nb < 0 && i == 0)
+// 			str[i--] = '-';
+// 		else
+// 		{
+// 			str[i--] = (n % 10) + '0';
+// 			n = n / 10;
+// 		}
+// 	}
+// 	return(write(1,str,aux));
+// }
 //Para el tamaño del string:
 size_t	ft_strlen(const char *str)
 {
@@ -26,95 +73,81 @@ size_t	ft_strlen(const char *str)
 		i++;
 	return (i);
 }
-//Para printear el string de forma "tradicional"
-int ft_printf_string(char const *str)
+//Para printear:
+int	ft_putchar(char c)
 {
-	return(write(1, str, ft_strlen(str)));
+	return(write(1, &c, 1));
 }
-//Para comprobar si es negativo:
-static int	ft_negnb(int nb)
+//Para los punteros:
+static int	ft_pointer(size_t num)
 {
-	if (nb < 0)
-		return (-nb);
-	return nb;
-}
-//Para ver el tamaño del número entero:
-static	int ft_nblen(int nb)
-{
-	int	len;
-
-	len = 0;
-	if (nb < 0)
-		len++;
-	while (nb !=  0)
-	{
-		nb /= 10;
-		len ++;
-	}
-	return len;
-}
-//Para printear el numero decimal en base 10
-int	ft_printf_d(int nb)
-{
-	char	*str;
-	int		i;
-	int		n;
-	int		aux;
-
-	i = ft_nblen(nb);
-	n = ft_negnb(nb);
-	aux = i;
-	str = (char *)malloc(sizeof(char) * (i + 1));
-	str[i--] = '\0';
-	while (i >= 0)
-	{
-		if (nb < 0 && i == 0)
-			str[i--] = '-';
-		else
-		{
-			str[i--] = (n % 10) + '0';
-			n = n / 10;
-		}
-	}
-	return(write(1,str,aux));
-}
-//Para el tema de las bases
-static int  ft_base(size_t nb, char *base)
-{
-    size_t  len;
+	size_t	len;
+	char	*base;
 
 	len = 1;
-    if (nb < 0)
-        nb += 4294967296;
-    if (nb >= (size_t)ft_strlen(base))
-    {
-        len += ft_base(nb / ft_strlen(base), base);
-        ft_putchar(base[nb % ft_strlen(base)]);
-    }
-    else
-        ft_putchar(base[nb]);
-    return (len);
+	base = "0123456789abcdef";
+	if (num >= (size_t)ft_strlen(base))
+	{
+		len += ft_pointer(num / ft_strlen(base));
+		ft_putchar(base[num % ft_strlen(base)]);
+	}
+	else
+		ft_putchar(base[num]);
+	return (len);
 }
-//Desde donde se distribuye todo:
-void ft_distributioner(char const format, va_list argv)
+
+static int	ft_print_ptr(void *ptr)
 {
-	char	*base_min;
+	size_t	ptr_num;
+	int		len;
+
+	len = 0;
+	ptr_num = (size_t)ptr;//Necesito una función aparte para aplicar la recursividad sin que falle a la hora de contar los bites
+	return (write(1, "0x", 2) + ft_pointer(ptr_num));
+}
+//Para el tema de las bases
+static int	ft_base(size_t num, char *base)
+{
+	size_t	len;
+
+	len = 1;
+	if (num >= (size_t)ft_strlen(base))
+	{
+		len += ft_base(num / ft_strlen(base), base);//Regresiva para que el num se vaya haciendo más pequeño y poder tener en cuenta el num de bytes que va a ocupar
+		ft_putchar(base[num % ft_strlen(base)]);//Para que vaya printeando el numero a la derecha
+	}
+	else
+		ft_putchar(base[num]);//Para cuando es menor que la base y es simplemente ese número
+	return (len);
+}
+//Desde donde se distribuyen los enteros:
+int	ft_distributor_num(int num, char format)
+{
+	char	*base_hex_min;
+	char	*base_hex_upp;
 	char	*base_dec;
 
+	base_hex_min = "0123456789abcdef";
+	base_hex_upp = "0123456789ABCDEF";
 	base_dec = "0123456789";
-	base_min = "0123456789abcdef";
-	if (format == 'c')
-		ft_putchar(va_arg(argv, int));
-	else if (format == 's')
-		ft_printf_string(va_arg(argv, char const *));
-	else if (format == 'd')
-		ft_printf_d(va_arg(argv, int));
-	else if (format == 'x')
-		ft_putchar(ft_base(va_arg(argv, int), base_min));
-	else if (format == 'u')
-		ft_putchar(ft_base(va_arg(argv, int), base_dec));
+	if (format == 'd' || format == 'i') //Para las condiciones donde los ints son los mismos:
+	{
+		if (num == -2147483648)//Tamañano mínimo negativo del int
+			return (write(1, "-2147483648", 11));
+		else if (num < 0)
+			return (ft_putchar('-') + ft_base(num * -1, base_dec));
+		else
+			return (ft_base(num, base_dec));
+	}
+	if (format == 'u')
+		return (ft_base((unsigned int)num, base_dec));
+	if (format == 'x')
+		return (ft_base((unsigned int)num, base_hex_min));
+	if (format == 'X')
+		return (ft_base((unsigned int)num, base_hex_upp));
+	return (1);//Para pensar señores ERROR
 }
-
+//LA tocha
 int	ft_printf(char const *format, ...)
 {
 	int i;
@@ -129,7 +162,8 @@ int	ft_printf(char const *format, ...)
 		if (*aux == '%')
 		{
 			aux++;
-			ft_distributioner(*aux, argv);
+			if (*aux == 'i'|| *aux == 'd' || *aux == 'u' || *aux == 'x' || *aux == 'X')
+				ft_distributor_num(va_arg(argv, int), *aux);
 			i++;
 		}
 		else
@@ -141,7 +175,7 @@ int	ft_printf(char const *format, ...)
 	return i;
 }
 
-
+//Main pruebitas
 int main()
 {
 	int	a;
@@ -157,5 +191,5 @@ int main()
 	b = 80;
 	d = -1723;
 	e = -1;
-	ft_printf("La primera letra de mi nommbre es:%c y la ultima es %c y la palabra es %s y el num es %d, y en hexa es: %x y el unsigned es %u", a, b, str, c, d, e);
+	ft_printf("La primera letra de \n mi nommbre es: y la ultima es y la palabra es y el num es %d, y en hexa es: %x y el unsigned es %u", c, d, e);
 }
